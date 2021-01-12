34fa6e68d1e9411c9736663d54ae522cfb2497ec
Fabio Petrone <fa610movic90@gmail.com>
2021-01-12 19:55:41 +0100

    piccole prove

--- main-bonus.js
+++ main-bonus.js
@@ -1,1 +1,37 @@
+// $('#piero').css({
+//
+//   'background-color':'red','color':'blue'});
 
+// $('#piero').css('color','blue');
+
+// in js puro è style.proprietà=""
+
+
+// $('#piero').click(function(){
+// var div=$('#totoggle');
+// if (div.is(":hidden"))
+// {
+// div.show(1000);
+// } else{
+//   div.hide(1000);
+// }
+//
+// })
+
+// $('#piero').click(function(){
+// var div=$('#totoggle');
+// if (div.is(":visible"))
+// {
+// div.hide(1000);
+// } else{
+//   div.show(1000);
+// }
+//
+// })
+
+// 
+// $('#piero').click(function(){
+// $('#totoggle').toggle(1000);
+//
+//
+// })
